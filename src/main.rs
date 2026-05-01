82d52339e0e917ce685cc544547a7432bc6a59cd
higor-esau <higor.esaut@gmail.com>
2026-05-01 19:41:48 -0400

    criando meu primeiro sistema em RAM ainda, mas funcional, algum erros de conceitos

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-use std::{net::ToSocketAddrs, sync::{Arc, Mutex}};
-use axum::{self, routing::get};
+use serde::{Deserialize, Serialize};
 
+
+#[derive(Deserialize, Serialize, Clone)]
 struct Donation{
     donor: String,
     amount: f64,
 }
 
+impl Donation{
+    fn valid(self: &Self) -> Result<(), String>{
+        match self {
+            x if x.amount < 0.0 => {
+                Err("amount invalido!".to_string())
+            }
+            x if x.donor.is_empty() => {
+                Err("Donor invalido!".to_string())
+            }
+            _ => {
+                Ok(())
+            }
+        }
+    }
+}
 struct AppState{
     total: f64,
     donations: Vec<Donation>,
@@ -15,7 +31,7 @@ struct AppState{
 #[tokio::main]
 async fn main() {
 
-    let mut state = AppState{
+    let state = AppState{
         total: 0.0,
         donations: vec![]
     };
@@ -23,8 +39,11 @@ async fn main() {
     let shared = std::sync::Arc::new(std::sync::Mutex::new(state));
 
     let app = axum::Router::new()
-    .route("/", get(home))
-    .route("/total", get(get_total));
+    .route("/", axum::routing::get(home))
+    .route("/total", axum::routing::get(get_total))
+    .route("/donate", axum::routing::post(donate))
+    .route("/donates", axum::routing::get(get_donates))
+    .with_state(shared);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -37,6 +56,27 @@ async fn home() -> String{
     "Sejá bem vindo a API do higor".to_string()
 }
 
-async fn get_total() -> String{
-    "Total = 0".to_string()
+async fn get_total(axum::extract::State(state): axum::extract::State<std::sync::Arc<std::sync::Mutex<AppState>>>) -> String{
+    let data = state.lock().unwrap();
+    format!("Total: {}", data.total)
+}
+
+async fn donate(
+    axum::extract::State(state): 
+    axum::extract::State<std::sync::Arc<std::sync::Mutex<AppState>>>, 
+    axum::Json(x): 
+    axum::Json<Donation>)
+     -> Result<String, String>{
+    let _ = x.valid()?;
+
+    let mut data = state.lock().unwrap();
+    data.total += x.amount;
+    let res = format!("Adicionando R$ {} de {}", x.amount, x.donor);
+    data.donations.push(x);
+    Ok(res)
+}
+
+async fn get_donates(axum::extract::State(state): axum::extract::State<std::sync::Arc<std::sync::Mutex<AppState>>>) ->axum::Json<Vec<Donation>>{
+    let data = state.lock().unwrap();
+    axum::Json(data.donations.clone())
 }
