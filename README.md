af250c90e81b486be268b4863b47722441b69a29
higor-esau <higor.esaut@gmail.com>
2026-05-01 16:54:52 -0400

    adicionando rota de / e /total

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# rust-donation-api
+# Rust Donation API
+
+API simples para gerenciamento de doações em Rust.
+
+## Objetivos
+- Trabalhar com estado compartilhado em memória
+- Criar rotas GET e POST
+- Receber e armazenar doações temporariamente
+- Praticar arquitetura básica de APIs
+
+## Funcionalidades atuais
+- POST /donate
+- GET /total
+- GET /donations
+
+## Stack utilizada
+- Rust
+- Axum
+- Tokio
+- Serde
+
+## Executando localmente
+
+```bash
+cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::ToSocketAddrs, sync::{Arc, Mutex}};
+use axum::{self, routing::get};
+
+struct Donation{
+    donor: String,
+    amount: f64,
+}
+
+struct AppState{
+    total: f64,
+    donations: Vec<Donation>,
+}
+
+
+#[tokio::main]
+async fn main() {
+
+    let mut state = AppState{
+        total: 0.0,
+        donations: vec![]
+    };
+
+    let shared = std::sync::Arc::new(std::sync::Mutex::new(state));
+
+    let app = axum::Router::new()
+    .route("/", get(home))
+    .route("/total", get(get_total));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Server running on http://localhost:3000");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn home() -> String{
+    "Sejá bem vindo a API do higor".to_string()
+}
+
+async fn get_total() -> String{
+    "Total = 0".to_string()
 }
